75e00a3f8a132ca1dce3d8c3ad2cc31378de105b
Somya Kothari <somyakothari454@gmail.com>
2025-08-26 05:33:37 -0700

    python basic programs

---
+++ Python_basics/Typecasting.py
@@ -0,0 +1,23 @@
+#Explicit typecasting
+#a is a string ,but the string value is valid integer
+# a="7"
+# print(int(a))
+# print(float(a))
+# print(complex(a))
+# print(bool(a))
+# print(str(a))
+
+#a is a string, but the string value is float
+a="7.7"
+print(float(a))
+print(int(float(a)))
+print(complex(a))
+print(bool(a))
+print(str(a))
+
+#implicit typecasting
+# a=7.1#Float
+# b=3#integer
+# print(a+b)
+#output=10.1
+
---
+++ Python_basics/user_input().py
@@ -0,0 +1,16 @@
+#...............How to take input from user.................#
+# a=input()
+# b=input()
+# print(int(a)+int(b))
+
+# Note:input value always considered to be a string or a character only 
+# ex:
+# a=input()
+# b=input()
+# print(a+b)
+
+#therefore we use typecasting:
+# ex:
+a=int(input())
+b=complex(input())
+print(a+b)
